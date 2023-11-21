ee29275ae4f2008c289bbde1b8f01b6580cd8946
shal01 <shalin001classics@gmail.com>
2023-11-21 16:56:16 +0530

    dynamic commit

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('',views.store,name='store'),
     path('checkout/',views.checkout,name='checkout'),
     path('cart/',views.cart,name='cart'),
 ]
+
+
--- store/views.py
+++ store/views.py
@@ -1,18 +1,41 @@
 from django.shortcuts import render
-from store.models import *
+from .models import *
 
-# Create your views here.
 
 def store(request):
     product = Product.objects.all()
     context = {'obj':product}
     return render(request,'store/store.html',context)
 
-def checkout(request):
-    context = {}
-    return render(request,'store/checkout.html')
+def main(request):
+    order = Order.objects.all()
+    context = {'order':order}
+    return render(request,'store/main.html',context)
+
 
 def cart(request):
-    context = {}
-    return render(request,'store/cart.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order , created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+    else : 
+        items = []
+        order = {'get_cart_items':0,'order.get_cart_total':0}
 
+    context = {'items':items,'order':order}
+    return render(request , 'store/cart.html',context)
+
+
+
+
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order ,created = Order.objects.get_or_create(customer=customer , complete = False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_items':0,'get_cart_total':0}
+
+    context = {'items':items,'order':order}
+    return render(request,'store/checkout.html',context)
